86547010ddadb801547dd07fe78cd7c045e9a7ff
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-01-17 18:53:04 -0300

    finshed lessons from first art class

--- scripts/scrawl.R
+++ scripts/scrawl.R
@@ -24,8 +24,12 @@ library(here)
 
 art_par <- list(
   seed = 2,
-  n_paths = 500,
-  n_steps = 80
+  n_paths = 700,
+  n_steps = 100,
+  sz_step = 150,
+  sz_slip = 100,
+  color = "steelblue",
+  palette = "inferno"
 )
 
 # setting up the canvas ---------------------------------------------------
@@ -43,7 +47,8 @@ state <- tibble(
     n = art_par$n_paths,
     min = 0,
     max = 2
-  )
+  ),
+  z = 0
 )
 
 
@@ -65,18 +70,83 @@ art_dat <- state
 stop_painting <- FALSE
 
 while (stop_painting == FALSE) {
+  # make a step
+
+  step <- curl_noise(
+    generator = gen_simplex,
+    x = state$x,
+    y = state$y,
+    z = state$z,
+    seed = c(1, 1, 1) * art_par$seed
+  )
+
+
   # do some painting
-  
-  state <- state %>% 
+
+  state <- state %>%
     mutate(
-      x = x + .1,
+      x = x + (step$x / 10000) * art_par$sz_step,
+      y = y + (step$y / 10000) * art_par$sz_step,
+      z = z + (step$z / 10000) * art_par$sz_slip,
       step_id = step_id + 1,
     )
-  
-  current_step <- last(state$step_id)
-  print(current_step)
-  
-  if(last(state$step_id) >= art_par$n_steps) {
+
+  # append state to art_dat
+
+  art_dat <- bind_rows(art_dat, state)
+
+  # check if we should stop?
+
+  if (last(state$step_id) >= art_par$n_steps) {
     stop_painting <- TRUE
   }
 }
+
+
+# draw the picture --------------------------------------------------------
+
+
+pic <-
+  ggplot(data = art_dat,
+         mapping = aes(
+           x = x,
+           y = y,
+           group = path_id,
+           color = step_id
+         )) +
+  geom_path(size = .5,
+            alpha = .5,
+            show.legend = FALSE) +
+  coord_equal() +
+  # scale_color_scico(palette = art_par$palette) +
+  scale_color_viridis_c(option = art_par$palette) +
+  theme_void() +
+  theme(
+    panel.background = element_rect(fill = art_par$color,
+                                    colour = art_par$color),
+    plot.background = element_rect(fill = art_par$color, color = art_par$color)
+  ) 
+
+print(pic)
+
+
+# save the ggplot to a png ------------------------------------------------
+
+pixels_wide <- 3000
+pixels_high <- 3000
+
+file_name <-
+  art_par %>%
+  str_c(collapse = "-") %>%
+  str_c("scrawl_", ., ".png")
+
+
+
+# ggsave(
+#   filename = file_name,
+#   path = here("img"),
+#   plot = pic,
+#   width = pixels_high / 300,
+#   height = pixels_wide / 300,
+#   dpi = 300
+# )
